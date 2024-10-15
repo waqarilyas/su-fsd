048ddace1315f6924dee86fb34c3589ee2631516
waqarilyas <waqariyas.dev@gmail.com>
2024-10-15 21:57:03 +0500

    refactor: Remove unused CSS properties and animations from ToggleTheme component

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,38 +1,20 @@
 import { Button } from "@headlessui/react";
 import { MoonIcon, SunIcon } from "@heroicons/react/20/solid";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 export const ToggleTheme = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <Button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
       className="flex items-center justify-center p-1"
     >
-      <div
-        className={`flex items-center gap-2 ${
-          theme === "dark" ? "hidden" : "flex"
-        }`}
-      >
+      <div className={`flex items-center gap-2 dark:hidden`}>
         <MoonIcon className="h-5 w-5" />
       </div>
 
-      <div
-        className={`flex items-center gap-2 ${
-          theme === "light" ? "hidden" : "flex"
-        }`}
-      >
+      <div className={`flex items-center gap-2 light:hidden`}>
         <SunIcon className="h-5 w-5" />
       </div>
     </Button>
